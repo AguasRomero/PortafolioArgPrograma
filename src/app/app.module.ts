a994039884cbc82ba718689f81400121a5226408
Agustín Romero <agustin@pcagus.fibertel.com.ar>
2022-05-21 20:33:24 -0300

    Committer: Agustín Romero <agustin@pcagus.fibertel.com.ar>  En la rama main  Tu rama está actualizada con 'origin/main'.  Cambios a ser confirmados: 	modificados:     README.md 	modificados:     angular.json 	modificados:     package-lock.json 	modificados:     package.json 	modificados:     src/app/app.component.css 	modificados:     src/app/app.component.html 	modificados:     src/app/app.module.ts 	modificados:     src/index.html 	modificados:     src/styles.css  Archivos sin seguimiento: 	src/app/componentes/ 	src/assets/AP-logo.png 	src/assets/banner.JPG

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { AppComponent } from './app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { CabezalComponent } from './componentes/cabezal/cabezal.component';
+import { AcercaDeComponent } from './componentes/acerca-de/acerca-de.component';
+import { ExperenciaComponent } from './componentes/experencia/experencia.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
+import { HabilidadesComponent } from './componentes/habilidades/habilidades.component';
+import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabezalComponent,
+    AcercaDeComponent,
+    ExperenciaComponent,
+    EducacionComponent,
+    HabilidadesComponent,
+    ProyectosComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NoopAnimationsModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
