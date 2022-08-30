93748c3eabbde4c5cc6c2674d13782a8d8f4a2ba
Agustín <romeroagustin54@gmail.com>
2022-08-30 17:40:15 -0300

    Perfil basico obtenido del BackEnd

--- FrontEnd/src/app/componentes/cabezal/cabezal.component.ts
+++ FrontEnd/src/app/componentes/cabezal/cabezal.component.ts
@@ -1,18 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import {MatDialog} from '@angular/material/dialog';
 import { AccesoComponent } from '../acceso/acceso.component';
+import { perfil } from 'src/app/modelo/perfil.model';
+import { PerfilService } from 'src/app/servicios/perfil.service';
+
 @Component({
   selector: 'app-cabezal',
   templateUrl: './cabezal.component.html',
   styleUrls: ['./cabezal.component.css']
 })
 export class CabezalComponent implements OnInit {
+  perfil: perfil = new perfil("","","","");
 
-  constructor(public dialog: MatDialog) { }
+  constructor(public dialog: MatDialog, public perfilService: PerfilService) { }
   abrirLogin(): void {
     this.dialog.open(AccesoComponent);
   }
   ngOnInit(): void {
+    this.perfilService.getPerfil().subscribe(datos => {this.perfil = datos})
   }
 
 }
